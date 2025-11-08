31c293c2051f136839d4e858d69c060372bf6ee2
Eder Abraham Sampayo Gonzalez <eder.sampayog@uanl.edu.mx>
2025-11-07 22:30:18 -0600

    añadida camara funcional
    puede mostrar la cámara y tomar fotos.
    La foto tomada se muestra abajo de los botones

--- App.js
+++ App.js
@@ -1,71 +1,172 @@
-import { StyleSheet, Text, View, Button, TextInput, FlatList, ListView } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {Platform} from 'react-native';
-import {Camera, CameraType} from 'expo-camera';
-import * as MediaLibrary  from 'expo-media-library';
+import { Platform } from 'react-native';
+//import * as MediaLibrary from 'expo-media-library';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { SafeAreaView } from 'react-native-web';
+import { File, Directory, Paths } from 'expo-file-system';
+import { PermissionsAndroid } from 'react-native';
+
 
 export default function App() {
 
-  const [permisos, setPermisos] = useState(null);
-  const [foto, setFoto] = useState(null);
-  const [tipoCamara, setTipoCamera] = useState('back');
-  const [camaraRef, setCamaraRef] = useState(null);
+  async function requestStoragePermission() {
+    try {
+      // Source - https://stackoverflow.com/a
+      // Posted by Nikitas IO, modified by community. See post 'Timeline' for change history
+      // Retrieved 2025-11-07, License - CC BY-SA 4.0
 
-
-  useEffect(()=> {
-    (async () => {
-      const permisosMediaLibrary = await MediaLibrary.requestPermissionsAsync();
-      const permisosCamara = await Camera.requestCameraPermissionsAsync();
-
-      setPermisos(permisosCamara.status === 'granted');
-      if(!permisosCamara.granted){
-        console('Se necesitan permisos para usar la camara');
+      let StoragePermissions = "granted";
+      // The permission exists only for Android API versions bigger than 33 (Android 13),
+      // we can assume it's always granted beforehand
+      if (Platform.Version >= 33) {
+        StoragePermissions = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_MEDIA_LOCATION,
+          {
+            title: "Storage Permission",
+            message: "This app needs access to your storage to save photos.",
+            buttonNeutral: "Ask Me Later",
+            buttonNegative: "Cancel",
+            buttonPositive: "OK",
+          }
+        );
       }
-    })();
-  }, []);
-
-
-  if(permisos === null || permisos === 'denied'){
-    return <Text>No se tienen permisos para acceder a camara</Text>
-  }
-
-  const tomarFoto = async () =>{
-    if(camaraRef){
-      try{
-        const datosFoto = await camaraRef.takePictureAsync();
-        setFoto(datosFoto.uri);
-        console.log(datosFoto);
-        //const asset = await MediaLibrary.createAssetAsync(datosFoto.uri);
-        //console.log('foto guardada en galeria', asset);
-      }catch(error){
-        console.log('error ' + error)
-      }
+      console.log("version de android: " + Platform.Version);
+      console.log("Storage Permissions:", StoragePermissions);
+    } catch (error) {
+      console.error("Error requesting storage permission:", error);
     }
   }
 
-  const guardarFoto = async  () => {
-    if(foto){
-      try{
-        //guardar foto en galeria
-      }
-      catch(error){}
+
+  // variables de expo-camera
+  const [facing, setFacing] = useState('back');
+
+  // Solicitar permisos de camara y galeria
+  const [permission, requestPermission] = useCameraPermissions();
+  //const [mediaPermission, requestMediaPermission] = MediaLibrary.usePermissions();
+
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  const cameraRef = useRef(null);
+
+  const [foto, setFoto] = useState (null);
+
+  if (!permission) {
+    // Camera or media permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text>No access to camera</Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+
+  //Cambia la camara de frente a trasera y viceversa
+  function cambiarCamara() {
+    setFacing( actual => (actual === 'back' ? 'front' : 'back'));
+  }
+
+  const onCameraReady = () => {
+    console.log('La camara esta lista');
+    setIsCameraReady(true);
+  }
+
+  function guardarFoto(uriFoto){
+    //cargar archivo de la foto
+    const archivo = new File(uriFoto);
+    const directorio = new Directory(Paths.document);
+
+    const destino = new File(Paths.document, 'captura.jpg');
+
+    directorio.create();
+
+    try{
+      archivo.copy(destino);
+      setFoto(destino);
+      console.log('Foto guardada en:', destino.uri);
+    } catch (error) {
+      console.error('Error al guardar la foto:', error);
     }
+  }
+
+  const tomarFoto = async () => {
+    if (isCameraReady ) {
+      const foto = await cameraRef.current.takePictureAsync();
+      setFoto(foto);
+      console.log('Foto tomada:', foto.uri);
+      //MediaLibrary.saveToLibraryAsync(foto.uri);
+      console.log('intentando guardar foto en la galeria');
+      //guardarFoto(foto.uri);
+      //Regresar una vista de la imagen tomada
+      //return (<SafeAreaView><Image source={{ uri: foto.uri }} style={{ flex: 1 }} /><Text>Foto tomada</Text></SafeAreaView>);
+      
+      // Guardar la foto en el sistema de archivos
+      destino = Directory.documentDirectory + 'foto.jpg';
+      console.log('Destino de la foto:' + destino);
+
+      try{
+        await Directory.copyAsync({
+          from: foto.uri,
+          to: destino,
+        });
+        console.log('Foto guardada en:', destino);
+
+      } catch (error) {
+        console.error('Error al guardar la foto:', error);
+      }
+    }
+    else{
+      console.log('La camara no esta lista');
+    }
+
+    if (!cameraRef.current) {
+      console.log('No se tiene referencia de la camara');
+      return;
+    }
+
   }
 
   return (
     <View style={styles.container}>
+      <CameraView
+        onCameraReady={onCameraReady}
+        style={styles.camera}
+        facing={facing}
+        ref ={cameraRef}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={cambiarCamara}>
+          <Text style={styles.text}> Cambiar Camara </Text>
+        </TouchableOpacity>
 
-   {permisos ? (
-     <Camera
-    type={tipoCamara}
-    ref={ref => setCamaraRef(ref)}>
+        <TouchableOpacity style={styles.button} onPress={tomarFoto}>
+          <Text style={styles.text}> Tomar Foto </Text>
+        </TouchableOpacity>
 
-    </Camera>
-   ) : (<Text>No access to camera</Text>)}
-    <Button title="Tomar Foto" onPress={tomarFoto}>Tomar Foto</Button>
-    <StatusBar style="auto" />
+        <TouchableOpacity style={styles.button} onPress={requestStoragePermission}>
+          <Text style={styles.text}> Solicitar Permiso de Almacenamiento </Text>
+        </TouchableOpacity>
+        {/* <Button title="Tomar Foto" onPress={tomarFoto}>Tomar Foto</Button> */}
+      </View>
 
+      <View>
+        {foto && (
+          <Image
+            source={{ uri: foto.uri }}
+            //si no carga la imagen, usar width y height en lugar de flex
+            style={{ width: 300, height: 400 }}
+
+          />
+        )}
+
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -79,7 +180,16 @@ const styles = StyleSheet.create({
     marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
   },
   camera: {
-    flex: 1,
-    aspectRatio:1
-  }
+    height: '60%',
+    width: '80%',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+  button: {
+    backgroundColor: '#BD93BD',
+    borderRadius: 5,
+    padding: 10,
+  },
 });
